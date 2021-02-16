0de67d8c11aefd6beb5534f5e17a7eb441777fa3
Alice Peng <720azpeng@gmail.com>
2021-02-16 08:57:41 -0500

    trying to organize plan better

---
+++ plan.py
@@ -0,0 +1,22 @@
+#Memory Game 
+
+## goals: GUI allows player to matches up the 8 different buttons 
+# with the other by selecting the buttons/image 
+# computer deletes counts correct and wrong and delete cards after selecting a correct pair
+
+#A simple memory game that employs GUI with 
+#layout of 16 cards(revealing themselves after you press the button) 
+#total with 8 different kind of card where the player trys to match up 
+#the cards by selecting a pair of buttons based on memory
+
+
+# create 16 buttons 
+# give attributes of: each holds image, buttons
+# give behaviors of: matching which partner image, show image when clicked
+
+
+
+# pair and remove 
+# show card if clicked card is shown and everytime we get a pair the pair is removed
+# suceed in fewest amount of attempts 
+
