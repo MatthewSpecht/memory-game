d5abaa7dd076df15ec0f1e9930c0673d4524f9da
Alice Peng <720azpeng@gmail.com>
2021-02-16 09:11:11 -0500

    trying to do shortcut for creating buttons

--- alice.py
+++ alice.py
@@ -14,22 +14,21 @@ my_frame.pack(pady=10)
 count = 0
 answer_list = []
 answer_dict ={} 
-b = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+b = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 def button_click(b, i):
     global count, answer_list, answer_dict
     if b["text"] == '' and count < 2:
         b["text"] = matches[i]
 
-# b0= tk.Button(my_frame, text='?',  font=("Helvetica", 20), bg = 'Red', width = 20, height = 10, command = lambda: button_click(b0, 0)
-#              ).grid(row=r, column=c, columnspan = 1, sticky = tk.W)    
-        
+# b0= tk.Button(my_frame, text='?',  font=("Helvetica", 20), bg = 'Red', width = 20, height = 10, command = lambda: button_click(b0, 0))
 
-for i in range(16):
+for i in matches:
     for r in range(4):
         for c in range(4):
-                "b" + str(i) = tk.Button(my_frame, text='?',  font=("Helvetica", 20), bg = 'Red', width = 20, height = 10, command = lambda: button_click(b + str(i), i)
-                ).grid(row=r, column=c, columnspan = 1, sticky = tk.W)   
+            #  "b" + str(i).grid(row=r, column=c, columnspan = 1, sticky = tk.W)             
+            "b" + str(i)=tk.Button(my_frame, text='?',  font=("Helvetica", 20), bg = 'Red', width = 20, height = 10, command = lambda: button_click("b" + str(i), i)
+                                  ).grid(row=r, column=c, columnspan = 1, sticky = tk.W)
 
                      
 
